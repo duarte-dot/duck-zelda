64829ebad788b528e8ba6033720609c0e601853c
Gabriel Duarte <gabrieldvr@outlook.com>
2024-11-28 12:20:21 -0300

    player collects gun

--- src/com/duartedot/entities/Entity.java
+++ src/com/duartedot/entities/Entity.java
@@ -1,131 +1,106 @@
 package com.duartedot.entities;
 
-import com.duartedot.main.Game;
-import com.duartedot.world.Camera;
-
 // import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
+import com.duartedot.main.Game;
+import com.duartedot.world.Camera;
+
 public class Entity {
 
-  public static BufferedImage WEAPON_EN = Game.spritesheet.getSprite(
-    7 * 16,
-    0,
-    16,
-    16
-  );
+	public static BufferedImage WEAPON_EN = Game.spritesheet.getSprite(7 * 16, 0, 16, 16);
 
-  public static BufferedImage LIFEPACK_EN = Game.spritesheet.getSprite(
-    8 * 16,
-    0,
-    16,
-    16
-  );
+	public static BufferedImage LIFEPACK_EN = Game.spritesheet.getSprite(8 * 16, 0, 16, 16);
 
-  public static BufferedImage BULLET_EN = Game.spritesheet.getSprite(
-    8 * 16,
-    16,
-    16,
-    16
-  );
+	public static BufferedImage BULLET_EN = Game.spritesheet.getSprite(8 * 16, 16, 16, 16);
 
-  public static BufferedImage ENEMY_EN = Game.spritesheet.getSprite(
-    7 * 16,
-    16,
-    16,
-    16
-  );
+	public static BufferedImage ENEMY_EN = Game.spritesheet.getSprite(7 * 16, 16, 16, 16);
 
-  protected double x;
-  protected double y;
-  protected int width;
-  protected int height;
+	public static BufferedImage GUN_LEFT = Game.spritesheet.getSprite(9 * 16, 16, 16, 16);
 
-  public BufferedImage sprite;
-  
-  private int maskx, masky, mwidth, mheight;
+	public static BufferedImage GUN_RIGHT = Game.spritesheet.getSprite(9 * 16, 0, 16, 16);
 
-  public Entity(int x, int y, int width, int height, BufferedImage sprite) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+	protected double x;
+	protected double y;
+	protected int width;
+	protected int height;
 
-    this.sprite = sprite;
+	public BufferedImage sprite;
 
-    this.maskx = 0;
-    this.masky = 0;
-    this.mwidth = width;
-    this.mheight = height;
-  }
+	private int maskx, masky, mwidth, mheight;
 
-  public void setMask(int maskx, int masky, int mwidth, int mheight) {
-    this.maskx = maskx;
-    this.masky = masky;
-    this.mwidth = mwidth;
-    this.mheight = mheight;
-  }
+	public Entity(int x, int y, int width, int height, BufferedImage sprite) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 
-  public int getX() {
-    return (int) x;
-  }
+		this.sprite = sprite;
 
-  public void setX(double x) {
-    this.x = x;
-  }
+		this.maskx = 0;
+		this.masky = 0;
+		this.mwidth = width;
+		this.mheight = height;
+	}
 
-  public int getY() {
-    return (int) y;
-  }
+	public void setMask(int maskx, int masky, int mwidth, int mheight) {
+		this.maskx = maskx;
+		this.masky = masky;
+		this.mwidth = mwidth;
+		this.mheight = mheight;
+	}
 
-  public void setY(double y) {
-    this.y = y;
-  }
+	public int getX() {
+		return (int) x;
+	}
 
-  public int getWidth() {
-    return width;
-  }
+	public void setX(double x) {
+		this.x = x;
+	}
 
-  public void setWidth(int width) {
-    this.width = width;
-  }
+	public int getY() {
+		return (int) y;
+	}
 
-  public int getHeight() {
-    return height;
-  }
+	public void setY(double y) {
+		this.y = y;
+	}
 
-  public void setHeight(int height) {
-    this.height = height;
-  }
+	public int getWidth() {
+		return width;
+	}
 
-  public void tick() {}
+	public void setWidth(int width) {
+		this.width = width;
+	}
 
-  public static boolean isColidding(Entity e1, Entity e2) {
-    Rectangle e1Mask = new Rectangle(
-      e1.getX() + e1.maskx,
-      e1.getY() + e1.masky,
-      e1.mwidth,
-      e1.mheight
-    );
+	public int getHeight() {
+		return height;
+	}
 
-    Rectangle e2Mask = new Rectangle(
-      e2.getX() + e2.maskx,
-      e2.getY() + e2.masky,
-      e2.mwidth,
-      e2.mheight
-    );
+	public void setHeight(int height) {
+		this.height = height;
+	}
 
-    return e1Mask.intersects(e2Mask);
-  }
+	public void tick() {
+	}
 
-  public void render(Graphics g) {
-    g.drawImage(sprite, this.getX() - Camera.x, this.getY() - Camera.y, null);
+	public static boolean isColidding(Entity e1, Entity e2) {
+		Rectangle e1Mask = new Rectangle(e1.getX() + e1.maskx, e1.getY() + e1.masky, e1.mwidth, e1.mheight);
 
-    // g.setColor(Color.RED);
-    // g.fillRect(this.getX() + maskx - Camera.x, this.getY() + masky - Camera.y, mwidth, mheight);
-  }
+		Rectangle e2Mask = new Rectangle(e2.getX() + e2.maskx, e2.getY() + e2.masky, e2.mwidth, e2.mheight);
 
-  
+		return e1Mask.intersects(e2Mask);
+	}
+
+	public void render(Graphics g) {
+		g.drawImage(sprite, this.getX() - Camera.x, this.getY() - Camera.y, null);
+
+		// g.setColor(Color.RED);
+		// g.fillRect(this.getX() + maskx - Camera.x, this.getY() + masky - Camera.y,
+		// mwidth, mheight);
+	}
+
 }
--- src/com/duartedot/entities/Player.java
+++ src/com/duartedot/entities/Player.java
@@ -1,189 +1,184 @@
 package com.duartedot.entities;
 
-import com.duartedot.graphics.Spritesheet;
-import com.duartedot.main.Game;
-import com.duartedot.world.Camera;
-import com.duartedot.world.World;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
+import com.duartedot.graphics.Spritesheet;
+import com.duartedot.main.Game;
+import com.duartedot.world.Camera;
+import com.duartedot.world.World;
+
 public class Player extends Entity {
 
-  public double life = 100;
-  public int maxLife = 100;
+	public double life = 100;
+	public int maxLife = 100;
 
-  public boolean right, left, up, down;
-  public int right_dir = 0, left_dir = 1;
-  public int dir = right_dir;
-  public double speed = 1.4;
+	public boolean right, left, up, down;
+	public int right_dir = 0, left_dir = 1;
+	public int dir = right_dir;
+	public double speed = 1.4;
 
-  private int frames = 0, maxFrames = 5, index = 0, maxIndex = 5;
-  private boolean moved = false;
+	private int frames = 0, maxFrames = 5, index = 0, maxIndex = 5;
+	private boolean moved = false;
 
-  private long lastTime; // Variável para controlar o tempo
-  private long delay = 500; // Delay de 0.5 segundos em milissegundos
+	private long lastTime; // Variável para controlar o tempo
+	private long delay = 500; // Delay de 0.5 segundos em milissegundos
 
-  private BufferedImage[] rightPlayer;
-  private BufferedImage[] leftPlayer;
+	private BufferedImage[] rightPlayer;
+	private BufferedImage[] leftPlayer;
 
-  private BufferedImage playerDamage;
+	private BufferedImage playerDamage;
 
-  public int ammo = 0;
+	private boolean hasGun = false;
 
-  public boolean isDamaged = false;
-  private int damageFrames = 0;
+	public int ammo = 0;
 
-  public Player(int x, int y, int width, int height, BufferedImage sprite) {
-    super(x, y, width, height, sprite);
-    rightPlayer = new BufferedImage[6];
-    leftPlayer = new BufferedImage[6];
+	public boolean isDamaged = false;
+	private int damageFrames = 0;
 
-    playerDamage = Game.spritesheet.getSprite(0, 32, 16, 16);
+	public Player(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+		rightPlayer = new BufferedImage[6];
+		leftPlayer = new BufferedImage[6];
 
-    for (int i = 0; i < 6; i++) {
-      rightPlayer[i] = Game.spritesheet.getSprite(0 + (i * 16), 0, 16, 16);
-    }
+		playerDamage = Game.spritesheet.getSprite(0, 32, 16, 16);
 
-    for (int i = 0; i < 6; i++) {
-      leftPlayer[i] = Game.spritesheet.getSprite(0 + (i * 16), 16, 16, 16);
-    }
+		for (int i = 0; i < 6; i++) {
+			rightPlayer[i] = Game.spritesheet.getSprite(0 + (i * 16), 0, 16, 16);
+		}
 
-    lastTime = System.currentTimeMillis();
-  }
+		for (int i = 0; i < 6; i++) {
+			leftPlayer[i] = Game.spritesheet.getSprite(0 + (i * 16), 16, 16, 16);
+		}
 
-  public void tick() {
-    moved = false;
-    double diagonalSpeed = (speed * Math.sqrt(2)) / 2;
+		lastTime = System.currentTimeMillis();
+	}
 
-    if (right && World.isFree(this.getX() + (int) speed, this.getY())) {
-      dir = right_dir;
-      moved = true;
-      setX(x + (right && up ? diagonalSpeed : speed));
-    } else if (left && World.isFree(this.getX() - (int) speed, this.getY())) {
-      dir = left_dir;
-      moved = true;
-      setX(x - (left && up ? diagonalSpeed : speed));
-    }
+	public void tick() {
+		moved = false;
+		double diagonalSpeed = (speed * Math.sqrt(2)) / 2;
 
-    if (up && World.isFree(this.getX(), (int) (y - speed))) {
-      moved = true;
-      setY(y - (up && (right || left) ? diagonalSpeed : speed));
-    } else if (down && World.isFree(this.getX(), (int) (y + speed))) {
-      moved = true;
-      setY(y + (down && (right || left) ? diagonalSpeed : speed));
-    }
+		if (right && World.isFree(this.getX() + (int) speed, this.getY())) {
+			dir = right_dir;
+			moved = true;
+			setX(x + (right && up ? diagonalSpeed : speed));
+		} else if (left && World.isFree(this.getX() - (int) speed, this.getY())) {
+			dir = left_dir;
+			moved = true;
+			setX(x - (left && up ? diagonalSpeed : speed));
+		}
 
-    long now = System.currentTimeMillis(); // Obtendo o tempo atual
-    long elapsedTime = now - lastTime; // Calculando o tempo decorrido desde a última atualização
+		if (up && World.isFree(this.getX(), (int) (y - speed))) {
+			moved = true;
+			setY(y - (up && (right || left) ? diagonalSpeed : speed));
+		} else if (down && World.isFree(this.getX(), (int) (y + speed))) {
+			moved = true;
+			setY(y + (down && (right || left) ? diagonalSpeed : speed));
+		}
 
-    if (moved) {
-      frames++;
+		long now = System.currentTimeMillis(); // Obtendo o tempo atual
+		long elapsedTime = now - lastTime; // Calculando o tempo decorrido desde a última atualização
 
-      if (frames == maxFrames) {
-        frames = 0;
-        index++;
+		if (moved) {
+			frames++;
 
-        if (index > maxIndex) {
-          index = 2;
-        }
-      }
-    } else {
-      // Se o tempo decorrido for maior ou igual ao delay, então atualiza o sprite
-      if (elapsedTime >= delay) {
-        index = (index == 0) ? 1 : 0;
-        frames = 0;
-        lastTime = now; // Atualizando o tempo da última atualização
-      }
-    }
+			if (frames == maxFrames) {
+				frames = 0;
+				index++;
 
-    checkItems();
+				if (index > maxIndex) {
+					index = 2;
+				}
+			}
+		} else {
+			// Se o tempo decorrido for maior ou igual ao delay, então atualiza o sprite
+			if (elapsedTime >= delay) {
+				index = (index == 0) ? 1 : 0;
+				frames = 0;
+				lastTime = now; // Atualizando o tempo da última atualização
+			}
+		}
 
-    if (isDamaged) {
-      damageFrames++;
+		checkItems();
 
-      if (damageFrames == 8) {
-        damageFrames = 0;
-        isDamaged = false;
-      }
-    }
+		if (isDamaged) {
+			damageFrames++;
 
-    if (life <= 0) {
-      Game.entities = new ArrayList<Entity>();
-      Game.enemies = new ArrayList<Enemy>();
+			if (damageFrames == 8) {
+				damageFrames = 0;
+				isDamaged = false;
+			}
+		}
 
-      Game.spritesheet = new Spritesheet("/spritesheet.png");
+		if (life <= 0) {
+			Game.entities = new ArrayList<Entity>();
+			Game.enemies = new ArrayList<Enemy>();
 
-      Game.player = new Player(0, 0, 16, 16, Game.spritesheet.getSprite(0, 0, 16, 16));
-      Game.world = new World("/map.png");
+			Game.spritesheet = new Spritesheet("/spritesheet.png");
 
-      Game.entities.add(Game.player);
-      return;
-    }
+			Game.player = new Player(0, 0, 16, 16, Game.spritesheet.getSprite(0, 0, 16, 16));
+			Game.world = new World("/map.png");
 
-    Camera.x = Camera.clamp(
-        this.getX() - (Game.WIDTH / 2),
-        0,
-        World.WIDTH * 16 - Game.WIDTH);
+			Game.entities.add(Game.player);
+			return;
+		}
 
-    Camera.y = Camera.clamp(
-        this.getY() - (Game.HEIGHT / 2),
-        0,
-        World.HEIGHT * 16 - Game.HEIGHT);
-  }
+		Camera.x = Camera.clamp(this.getX() - (Game.WIDTH / 2), 0, World.WIDTH * 16 - Game.WIDTH);
 
-  public void checkItems() {
-    for (int i = 0; i < Game.entities.size(); i++) {
-      Entity e = Game.entities.get(i);
+		Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT / 2), 0, World.HEIGHT * 16 - Game.HEIGHT);
+	}
 
-      if (e instanceof LifePack) {
-        if (Entity.isColidding(this, e)) {
-          life += 100;
+	public void checkItems() {
+		for (int i = 0; i < Game.entities.size(); i++) {
+			Entity e = Game.entities.get(i);
 
-          if (life >= maxLife) {
-            life = maxLife;
-          }
+			if (e instanceof LifePack) {
+				if (Entity.isColidding(this, e)) {
+					life += 100;
 
-          Game.entities.remove(i);
-          return;
-        }
-      } else if (e instanceof Bullet) {
-        if (Entity.isColidding(this, e)) {
-          ammo++;
-          Game.entities.remove(i);
-          return;
-        }
-      }
-    }
-  }
+					if (life >= maxLife) {
+						life = maxLife;
+					}
 
-  public void render(Graphics g) {
-    if (!isDamaged) {
-      if (dir == right_dir) {
-        g.drawImage(
-            rightPlayer[index],
-            this.getX() - Camera.x,
-            this.getY() - Camera.y,
-            null);
-      } else if (dir == left_dir) {
-        g.drawImage(
-            leftPlayer[index],
-            this.getX() - Camera.x,
-            this.getY() - Camera.y,
-            null);
-      } else {
-        g.drawImage(
-            rightPlayer[0],
-            this.getX() - Camera.x,
-            this.getY() - Camera.y,
-            null);
-      }
-    } else {
-      g.drawImage(
-          playerDamage,
-          this.getX() - Camera.x,
-          this.getY() - Camera.y,
-          null);
-    }
-  }
+					Game.entities.remove(i);
+					return;
+				}
+			} else if (e instanceof Bullet) {
+				if (Entity.isColidding(this, e)) {
+					ammo++;
+					Game.entities.remove(i);
+					return;
+				}
+			} else if (e instanceof Weapon) {
+				if (Entity.isColidding(this, e)) {
+					hasGun = true;
+					Game.entities.remove(i);
+					return;
+				}
+			}
+		}
+	}
+
+	public void render(Graphics g) {
+		if (!isDamaged) {
+			if (dir == right_dir) {
+				g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+
+				if (hasGun) {
+					g.drawImage(Entity.GUN_RIGHT, this.getX() - Camera.x + 8, this.getY() - Camera.y, null);
+				}
+			} else if (dir == left_dir) {
+				g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+
+				if (hasGun) {
+					g.drawImage(Entity.GUN_LEFT, this.getX() - Camera.x - 8, this.getY() - Camera.y, null);
+				}
+			} else {
+				g.drawImage(rightPlayer[0], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			}
+		} else {
+			g.drawImage(playerDamage, this.getX() - Camera.x, this.getY() - Camera.y, null);
+		}
+	}
 }
